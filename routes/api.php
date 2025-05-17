32c3bd10761f860a45fb73a2790a407ee73311ef
nourahaies <153508635+nourahaies@users.noreply.github.com>
2025-05-17 17:33:05 +0300

    notesCalendar+showForAll+up

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,11 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BuildingController;
+use App\Http\Controllers\CalendarNoteController;
 use App\Http\Controllers\CustodyController;
 use App\Http\Controllers\EntryNoteController;
+use App\Http\Controllers\ExitNoteController;
+use App\Http\Controllers\MaterialRequestController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\WarehouseController;
@@ -25,14 +28,20 @@ Route::middleware('auth:sanctum')->group(function () {
 
 
     //ENTRY NOTE
+
     Route::get('/allEntryNote',[EntryNoteController::class,'index']);
     Route::post('/entryNote',[EntryNoteController::class,'store']);
+
+    //EXIT NOTE
+    Route::get('/allExitNote',[ExitNoteController::class,'index']);
+    Route::post('/exitNote',[ExitNoteController::class,'store']);
 
     //    WAREHOUSE
     Route::post('/warehouses/store', [WarehouseController::class, 'store']);
     Route::put('/warehouses/update/{id}', [WarehouseController::class, 'update']);
     Route::delete('/warehouses/destroy/{id}', [WarehouseController::class, 'destroy']);
     Route::get('/warehouses/index', [WarehouseController::class, 'index']);
+    Route::get('warehouses/show/{id}', [WarehouseController::class, 'show']);
 
 
     //    PRODUCTS
@@ -40,15 +49,28 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('products/update/{id}', [ProductController::class, 'update']);
     Route::delete('products/delete/{id}', [ProductController::class, 'destroy']);
     Route::get('/products',[ProductController::class,'index']);
+    Route::get('products/show/{id}', [ProductController::class, 'show']);
+
+
+    //MATERIAL REQUEST
+    Route::post('/MRequest',[MaterialRequestController::class,'store']);
+    Route::get('/allRequestMaterial',[MaterialRequestController::class,'index']);
+    Route::get('/pendingRequestMaterial',[MaterialRequestController::class,'pendingRequests']);
+    Route::put('/materialRequests/{id}/edit', [MaterialRequestController::class, 'editRequest']);
+    Route::put('/materialRequests/{id}/reject', [MaterialRequestController::class, 'rejectRequest']);
+    Route::put('/materialRequests/{id}/approve', [MaterialRequestController::class, 'approveRequest']);
+
+
 
     //CUSTODY
     Route::post('/custody/store', [CustodyController::class, 'store']);
 
-    //  BUILDINGS
+    //BUILDINGS
     Route::post('/buildings/store', [BuildingController::class, 'store']);
     Route::put('buildings/update/{id}', [BuildingController::class, 'update']);
     Route::delete('buildings/delete/{id}', [BuildingController::class, 'destroy']);
     Route::get('/buildings',[BuildingController::class,'index']);
+    Route::get('buildings/show/{id}', [BuildingController::class, 'show']);
 
 
     //ROOMS
@@ -56,5 +78,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('rooms/update/{id}', [RoomController::class, 'update']);
     Route::delete('rooms/delete/{id}', [RoomController::class, 'destroy']);
     Route::get('/rooms',[RoomController::class,'index']);
-});
+    Route::get('rooms/show/{id}', [RoomController::class, 'show']);
 
+    //CALENDAR
+    Route::get('/calendar', [CalendarNoteController::class, 'index']);
+    Route::post('/calendar/store', [CalendarNoteController::class, 'store']);
+    Route::get('/calendar/show/{date}', [CalendarNoteController::class, 'show']);
+    Route::put('/calendar/update/{date}', [CalendarNoteController::class, 'update']);
+    Route::delete('/calendar/destroy/{date}', [CalendarNoteController::class, 'destroy']);
+
+});
